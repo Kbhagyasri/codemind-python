de7716e775bbe1e3164733af7fcb06c3362cb6b5
Kbhagyasri <119720683+Kbhagyasri@users.noreply.github.com>
2023-08-18 15:25:06 +0530

    Num1,Num2,Num3 = map(int,input().split()) if(Num1>Num2 and Num1>Num3):    print(f"{Num1}") elif(Num2>Num1 and Num2>Num3):     print(f"{Num2}") else:     print(f"{Num3}")

---
+++ Largest_number_among_3_numbers.py
@@ -0,0 +1,9 @@
+Num1,Num2,Num3 = map(int,input().split())
+if(Num1>Num2 and Num1>Num3):
+   print(f"{Num1}")
+elif(Num2>Num1 and Num2>Num3):
+    print(f"{Num2}")
+else:
+    print(f"{Num3}")
+    
+  
